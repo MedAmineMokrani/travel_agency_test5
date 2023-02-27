d016ef16177bb9528578fb363f3afb828e6b08c3
MOKRANI Mohamed Amine <mokrani@et.esiea.fr>
2023-02-27 16:49:03 +0100

    permission add

---
+++ site/src/test/java/fr/lernejo/travelsite/TravelControllerTest.java
@@ -0,0 +1,14 @@
+package fr.lernejo.travelsite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TravelControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void postInscription() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTravels() {
+    }
+}
