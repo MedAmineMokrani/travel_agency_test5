a550826807de941acb9e3a7cbfddb099365fd554
MOKRANI Mohamed Amine <mokrani@et.esiea.fr>
2023-02-27 21:51:33 +0100

    test push

--- site/src/test/java/fr/lernejo/travelsite/TravelControllerTest.java
+++ site/src/test/java/fr/lernejo/travelsite/TravelControllerTest.java
@@ -1,14 +1,50 @@
 package fr.lernejo.travelsite;
 
-import static org.junit.jupiter.api.Assertions.*;
+import fr.lernejo.travelsite.TravelInscription;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
 class TravelControllerTest {
 
-    @org.junit.jupiter.api.Test
-    void postInscription() {
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private TravelController travelController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(travelController).build();
     }
 
-    @org.junit.jupiter.api.Test
-    void getTravels() {
+    @Test
+    void testGetTravels() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/travels")
+                .param("userName", "test"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].country").value("France"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].temperature").isNumber())
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].country").value("Spain"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].temperature").isNumber());
+    }
+
+    @Test
+    void testPostInscription() throws Exception {
+        TravelInscription inscription = new TravelInscription("test", "test@gmail.com", "France",1, TravelInscription.WeatherExpectation.COLDER);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/inscription")
+                .contentType("application/json")
+                .content(inscription.toString()))
+            .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
